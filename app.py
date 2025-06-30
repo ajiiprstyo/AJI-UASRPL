d504ae658b389e2de4d1eca930e915a03dd4f796
ajiiprstyo <offcajistd88@gmail.com>
2025-06-30 09:41:17 +0000

    Mengubah terserah

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ def main():
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about you?'
+    return 'I am good broo, how about you?'
+
+@app.route('/about')
+def about():
+    return 'This is an about page.'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
